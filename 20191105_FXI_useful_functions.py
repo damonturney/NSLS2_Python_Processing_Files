3431252d7437d8bef15d6ba2d1c35338a59fd3d3
Damon Turney <damonturney@gmail.com>
2020-04-03 22:04:00 -0400

    202004032203: better code

--- 20191105_FXI_useful_functions.py
+++ 20191105_FXI_useful_functions.py
@@ -69,7 +69,7 @@ object_list_filenames_tiffiles = list(object_recursiveglob_tiffiles)
 
 
 ### Workflow ##############################################
-# 1) Run internally_align_h5_file(34567,[50,350,200,200]) on each Manganese multipos_2D_xanes_scan2_[]...h5 file to align the images. Use a command like for i in range(34675,34725,2): create_aligned_h5_file(i);      NOTE:  file 34675 is missing, see your beamline notes -- before 34675 the Mn files are odd numbered and after 34675 the Mn files are even numbered 
+# 1) Run internally_align_h5_file(file_number,[50,350,300,300],[50,100,75,75])  on each Manganese multipos_2D_xanes_scan2_[]...h5 file to align the images. Use a command like for i in range(34675,34725,2): create_aligned_h5_file(i);      NOTE:  file 34675 is missing, see your beamline notes -- before 34675 the Mn files are odd numbered and after 34675 the Mn files are even numbered .   the [50,350,300,300] chops off L.R.T.B. which have the copper TEM mesh which confuses the cc_image. The  [50,100,75,75] is how far to search in each direction when calculating the cross correlations
 # 2) Run align_processed_images_time_series([34567,34569],[50,350,200,200], []) on each of the processed_images...h5 fiels created in step 1.  The im2_cropping=[50,350,200,200] is how much of the sides and top/bottom to cutoff im2.    Use a command like for i in range(34675,34725,2): calculate_optical_thickness(i);   
 # 3) 
 ############################################################
@@ -130,7 +130,7 @@ def internally_align_h5_file(Mn_filename, im2_cropping, cc_search_distance):    
     ims_buffered_aligned = np.stack((Mn_im1_buffered,Mn_im2_buffered_aligned,Cu_im1_buffered_aligned,Cu_im2_buffered_aligned))
     
     # Stuff the data into the h5 file
-    h5object_new.create_dataset('translations', shape=(3,2),   dtype=np.float64, data=np.stack((translation1,translation2,translation3)))
+    h5object_new.create_dataset('translations_internal', shape=(3,2),   dtype=np.float64, data=np.stack((translation1,translation2,translation3)))
     h5object_new.create_dataset('beam_energies', shape=(4,1), dtype=np.float64, data=beam_energies)
     h5object_new.create_dataset('xray_images', shape=(4 , Mn_ims[0,:,:].shape[0] + 2*buffer_edges , Mn_ims[0,:,:].shape[1] + 2*buffer_edges), dtype=np.float32, data=ims_buffered_aligned)
     h5object_old.close()
@@ -140,7 +140,7 @@ def internally_align_h5_file(Mn_filename, im2_cropping, cc_search_distance):    
     
 
 #filename MUST be supplied as a numpy vector of file numbers
-def align_processed_images_time_series(file_numbers,cc_search_distance):  #filename MUST be supplied as a numpy vector of file numbers
+def align_processed_images_time_series(file_numbers,im2_cropping, cc_search_distance):  #filename MUST be supplied as a numpy vector of file numbers
     for i in range(1,len(file_numbers)):
         
         print(file_numbers[i])
@@ -186,7 +186,8 @@ def align_processed_images_time_series(file_numbers,cc_search_distance):  #filen
         print(translation3, error3)
         print(translation4, error4)
         print(translation)
-        
+        h5object_new.create_dataset('translations_time_series', shape=(2), dtype=np.float64, data=translation)
+
         # Now actually shift the images to be in alignment
         im2_1 = shift_image_integer(xanes_raw_ims2[0,:,:], -translation1)
         im2_2 = shift_image_integer(xanes_raw_ims2[1,:,:], -translation2)
