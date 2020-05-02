74b1112036c1532c3f7f403261cff65195c046cd
Damon Turney <damonturney@gmail.com>
2020-05-01 22:00:03 -0400

    202005012042: better code

--- 20191105_FXI_useful_functions.py
+++ 20191105_FXI_useful_functions.py
@@ -538,7 +538,7 @@ def make_movie_with_potentiostat_data(txm_scan_numbers,biologic_file, image_used
     # The animation uses FFMPEG (https://www.ffmpeg.org/ffmpeg-codecs.html#libvpx) to access video codecs.  I think by default it uses the h.264.  vpxenc (called libvpx by FFMPEG) which is explained here https://www.webmproject.org/docs/encoder-parameters/
     animation_handle=matplotlib.animation.FuncAnimation(fig_han, change_imshow, frames=range(int(movie_time_span_seconds/seconds_per_movie_frame)), blit=False, interval=100, repeat=False)
     Writer = matplotlib.animation.writers['ffmpeg']
-    writer = Writer(fps=15, codec='libx265')#, extra_args=[ 'lossless', '`'])
+    writer = Writer(fps=15, codec='hevc', extra_args=[ '-lossless', '1'])
     animation_handle.save(output_filename, writer=writer)
 
 
